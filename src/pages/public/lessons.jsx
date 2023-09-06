204b1bdb1febbfb3bff4e8a373f5e761e9b7195d
Savallera <phenixvn1985@gmail.com>
2023-09-06 18:43:01 +0300

    add index and lessons page on public

--- src/pages/public/lessons.jsx
+++ src/pages/public/lessons.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Lessons from "../../components/public/lessons/lessons";
+import Feedback from "../../components/public/feedback/feedback";
 
-const MainPage = () => {
+const LessonsPage = () => {
     return (
         <>
             <Lessons />
+            <Feedback />
         </>
     );
 };
 
-export default MainPage;
+export default LessonsPage;
