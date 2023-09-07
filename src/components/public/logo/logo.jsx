cb1ac2668d77d9ebd88ec69cf0c6ae1893aae82c
Navat2007 <navat2007@yandex.ru>
2023-09-07 11:34:33 +0300

    add

--- src/components/public/logo/logo.jsx
+++ src/components/public/logo/logo.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./logo.scss";
 import logo from "../../../images/logo.svg";
+import {NavLink} from "react-router-dom";
 
 const Logo = ({ place }) => {
     return (
-        <a className={`logo ${place && `logo_place_${place}`}`} href='/' rel='noreferer nofollow noopener'>
+        <NavLink
+            to={"/"}
+            className={`logo ${place && `logo_place_${place}`}`}
+            rel='noreferer nofollow noopener'
+            aria-label={"Главная страница"}
+        >
             <img src={logo} alt='Векторное изображение трех сосен с надписью - Сосны' />
-        </a>
+        </NavLink>
     );
 };
 
