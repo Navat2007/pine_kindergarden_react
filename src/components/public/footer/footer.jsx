f1b3bf99ed3eef5dd1da5315b1adeb41eaaf638c
Savallera <phenixvn1985@gmail.com>
2023-09-07 12:59:15 +0300

    add lk button to footer

--- src/components/public/footer/footer.jsx
+++ src/components/public/footer/footer.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./footer.scss";
 import Menu from "../menu/menu";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
     return (
         <footer className='footer main-section section_type_fixed'>
-            <div className='footer__wrap'>
+            <div className='footer__inner'>
                 <Menu place={"footer"} logoPlace={"footer"} />
-                <p className='footer__author'>Детский&nbsp;сад&nbsp;СОСНЫ Все&nbsp;права&nbsp;защищены</p>
+                <div className='footer__content'>
+                    <p className='footer__author'>Детский&nbsp;сад&nbsp;СОСНЫ Все&nbsp;права&nbsp;защищены</p>
+                    <NavLink to={"/login"} className={"footer__lk-link main-link"}>
+                        Личный кабинет
+                    </NavLink>
+                </div>
             </div>
         </footer>
     );
