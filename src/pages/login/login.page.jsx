3cd99974b8f8abfef5ed4ac52790287fef2ccd67
Savallera <phenixvn1985@gmail.com>
2023-09-07 08:47:25 +0300

    test commit

--- src/pages/login/login.page.jsx
+++ src/pages/login/login.page.jsx
@@ -25,67 +25,62 @@ const LoginPage = () => {
     };
 
     return (
-        <Popup
-            opened={true}
-            title={"Окно входа"}
-            extraClass={styles.popup_login}
-        >
-            <form
-                onSubmit={handleSubmit(onSubmit)}
-                className={`form ${styles.form}`}
-            >
-                <FieldInput
-                    placeholder={"Введите логин..."}
-                    {...register("login", {
-                        required: "Поле обязательно к заполнению",
-                    })}
-                    defaultValue={
-                        window.localStorage.getItem("remember") &&
-                        window.localStorage.getItem("login")
-                            ? window.localStorage.getItem("login")
-                            : ""
-                    }
-                    errorText={errors?.login?.message}
-                />
-                <FieldInput
-                    type={"password"}
-                    placeholder={"Введите пароль..."}
-                    {...register("password", {
-                        required: "Поле обязательно к заполнению",
-                    })}
-                    defaultValue={
-                        window.localStorage.getItem("remember") &&
-                        window.localStorage.getItem("pwd")
-                            ? window.localStorage.getItem("pwd")
-                            : ""
-                    }
-                    errorText={errors?.password?.message}
-                />
-                <FieldInput
-                    type={"checkbox"}
-                    label={"Запомнить логин и пароль?"}
-                    {...register("remember")}
-                    defaultChecked={window.localStorage.getItem("remember")}
-                />
-                <p
-                    className={`${styles.info} ${
-                        error ? styles.info_actived : ""
-                    }`}
-                >
-                    {errorText}
-                </p>
-                <Button
-                    type="submit"
-                    text={"Войти"}
-                    disabled={loading === "loading"}
-                    spinnerActive={loading === "loading"}
-                />
-                {/*<div className={styles.navigation}>*/}
-                {/*    <Link className={styles.link}>Забыли пароль?</Link>*/}
-                {/*    <Link className={styles.link}>Помощь</Link>*/}
-                {/*</div>*/}
-            </form>
-        </Popup>
+        <>
+        <main>
+            <section>
+                <h1>Рады приветствовать!</h1>
+                <form action="" name="">
+
+                </form>
+            </section>
+        </main>
+            <Popup opened={true} title={"Окно входа"} extraClass={styles.popup_login}>
+                <form onSubmit={handleSubmit(onSubmit)} className={`form ${styles.form}`}>
+                    <FieldInput
+                        placeholder={"Введите логин..."}
+                        {...register("login", {
+                            required: "Поле обязательно к заполнению",
+                        })}
+                        defaultValue={
+                            window.localStorage.getItem("remember") && window.localStorage.getItem("login")
+                                ? window.localStorage.getItem("login")
+                                : ""
+                        }
+                        errorText={errors?.login?.message}
+                    />
+                    <FieldInput
+                        type={"password"}
+                        placeholder={"Введите пароль..."}
+                        {...register("password", {
+                            required: "Поле обязательно к заполнению",
+                        })}
+                        defaultValue={
+                            window.localStorage.getItem("remember") && window.localStorage.getItem("pwd")
+                                ? window.localStorage.getItem("pwd")
+                                : ""
+                        }
+                        errorText={errors?.password?.message}
+                    />
+                    <FieldInput
+                        type={"checkbox"}
+                        label={"Запомнить логин и пароль?"}
+                        {...register("remember")}
+                        defaultChecked={window.localStorage.getItem("remember")}
+                    />
+                    <p className={`${styles.info} ${error ? styles.info_actived : ""}`}>{errorText}</p>
+                    <Button
+                        type='submit'
+                        text={"Войти"}
+                        disabled={loading === "loading"}
+                        spinnerActive={loading === "loading"}
+                    />
+                    {/*<div className={styles.navigation}>*/}
+                    {/*    <Link className={styles.link}>Забыли пароль?</Link>*/}
+                    {/*    <Link className={styles.link}>Помощь</Link>*/}
+                    {/*</div>*/}
+                </form>
+            </Popup>
+        </>
     );
 };
 
