b0c9ea1b60424b8b892546d07241e5f539adf32a
Navat2007 <navat2007@yandex.ru>
2023-09-08 15:24:20 +0300

    add

--- src/components/public/feedback/feedback.jsx
+++ src/components/public/feedback/feedback.jsx
@@ -1,62 +1,87 @@
 import React from "react";
-
 import { useForm } from "react-hook-form";
+
+import useFeedbackStore from "../../../store/feedbackStore";
+
+import Notif from "../../general/notif/notif.component";
 
 import "./feedback.scss";
 
 const Feedback = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
+    const [notif, setNotif] = React.useState(<></>);
+    const {register, handleSubmit, setFocus} = useForm();
+    const feedbackStore  = useFeedbackStore();
 
     const onSubmit = async (data) => {
+        await feedbackStore.sendFeedback(data);
 
+        setNotif(
+            <Notif
+                text='Запрос успешно отправлен'
+                state='success'
+                timerInSeconds={3}
+                opened={true}
+                onClose={() => setNotif(<></>)}
+            />
+        );
     };
 
     return (
-        <section className='feedback main-section section_type_fixed'>
-            <div className='feedback__inner'>
-                <div className='feedback__description'>
-                    <p className='feedback__description-text'>
-                        Хотите сделать своего ребенка счастливым? <br />
-                        Приходите&nbsp;к&nbsp;нам
-                    </p>
-                    <button className='feedback__description-button' type='button' aria-label='Записаться'>
-                        <span>Записаться</span>
-                    </button>
-                </div>
-                <form className='feedback__form' onSubmit={handleSubmit(onSubmit)}>
-                    <h2 className='feedback__title'>Закажите обратный звонок</h2>
-                    <input
-                        id='phone'
-                        className='feedback__field'
-                        type='tel'
-                        name='phone'
-                        autoComplete='phone'
-                        required
-                        placeholder='+7(___)___-__-__'
-                        pattern='\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}'
-                    />
-                    <textarea
-                        id='message'
-                        className='feedback__field'
-                        rows='4'
-                        name='message'
-                        placeholder='Ваше сообщение'
-                    />
-                    <div className='feedback__checkbox-box'>
-                        <input type='checkbox' id='privacy' required />
-                        <label htmlFor='privacy'>Я согласен на обработку моих данных</label>
+        <>
+            <section className='feedback main-section section_type_fixed'>
+                <div className='feedback__inner'>
+                    <div className='feedback__description'>
+                        <p className='feedback__description-text'>
+                            Хотите сделать своего ребенка счастливым? <br />
+                            Приходите&nbsp;к&nbsp;нам
+                        </p>
+                        <button
+                            className='feedback__description-button'
+                            type='button'
+                            aria-label='Записаться'
+                            onClick={() => {
+                                setFocus("phone");
+                            }}
+                        >
+                            <span>Записаться</span>
+                        </button>
                     </div>
-                    <span className='feedback__span-text'>Необходимо заполнить номер телефона</span>
-                    <button className='feedback__button' type='submit' aria-label='Заказать'>
-                        Заказать
-                    </button>
-                </form>
-            </div>
-        </section>
+                    <form className='feedback__form' onSubmit={handleSubmit(onSubmit)}>
+                        <h2 className='feedback__title'>Закажите обратный звонок</h2>
+                        <input
+                            id='phone'
+                            className='feedback__field'
+                            type='tel'
+                            name='phone'
+                            autoComplete='phone'
+                            required
+                            placeholder='+7(___)___-__-__'
+                            pattern='\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}'
+                            {...register("phone", {
+                                required: "Поле обязательно к заполнению",
+                            })}
+                        />
+                        <textarea
+                            id='message'
+                            className='feedback__field'
+                            rows='4'
+                            name='message'
+                            placeholder='Ваше сообщение'
+                            {...register("message")}
+                        />
+                        <div className='feedback__checkbox-box'>
+                            <input type='checkbox' id='privacy' required />
+                            <label htmlFor='privacy'>Я согласен на обработку моих данных</label>
+                        </div>
+                        <span className='feedback__span-text'>Необходимо заполнить номер телефона</span>
+                        <button className='feedback__button' type='submit' aria-label='Заказать'>
+                            Заказать
+                        </button>
+                    </form>
+                </div>
+            </section>
+            {notif}
+        </>
     );
 };
 
---
+++ src/store/feedbackStore.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import create from 'zustand'
+
+const urlSendFeedback = process.env.REACT_APP_BASE_URL + 'php/feedback.php';
+
+const useFeedbackStore = create(
+    (set, get) => ({
+        loading: false,
+        sending: false,
+
+        error: false,
+        errorText: "",
+        setErrorText: (text) => {
+            set({error: true, errorText: text});
+        },
+        clearErrorText: () => {
+            set({error: false, errorText: ""});
+        },
+
+        sendFeedback: async (params) => {
+            set({sending: true});
+
+            let form = new FormData();
+            window.global.buildFormData(form, params);
+
+            const response = await axios.postForm(urlSendFeedback, form);
+
+            set({sending: false});
+
+            console.log(response.data);
+        },
+    })
+);
+
+export default useFeedbackStore;
