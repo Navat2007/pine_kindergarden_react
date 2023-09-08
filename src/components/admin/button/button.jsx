3ae67d50485db24206def5f3c36c789529b7caf3
Navat2007 <navat2007@yandex.ru>
2023-09-08 12:15:43 +0300

    add

--- src/components/admin/button/button.jsx
+++ src/components/admin/button/button.jsx
@@ -8,7 +8,6 @@ const Button = ({type = "submit", text, spinnerActive = false, ...rest}) => {
         <motion.button
             className='button'
             whileTap={{scale: 0.97}}
-            whileHover={{scale: 1.03}}
             transition={{type: "spring", stiffness: 400, damping: 20}}
             type={type}
             {...rest}
