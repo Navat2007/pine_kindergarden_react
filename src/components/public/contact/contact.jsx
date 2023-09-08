457efc4a1abe9f42a4ab42f5c154df4226fc045a
Navat2007 <navat2007@yandex.ru>
2023-09-08 12:26:33 +0300

    add

--- src/components/public/contact/contact.jsx
+++ src/components/public/contact/contact.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { YMaps, Map, Placemark } from "react-yandex-maps";
+
 import "./contact.scss";
 
 const Contact = () => {
+
+
+
     return (
         <section className='contact main-section'>
-            <div className='contact__map' id='contact_map'></div>
+            <div className='contact__map' id='contact_map'>
+                <YMaps>
+                    <Map
+                        state={{
+                            center: [55.762373, 37.607898],
+                            zoom: 16,
+                        }}
+                        width='100%'
+                        height='100%'
+                    >
+                        <Placemark geometry={[55.762373, 37.607898]} properties={{iconContent: "Детский сад СОСНЫ"}} options={{preset: 'islands#redStretchyIcon', }}/>
+                    </Map>
+                </YMaps>
+            </div>
             <div className='contact__detail'>
                 <h2 className='contact__title'>Контакты</h2>
                 <address className='contact__address'>
