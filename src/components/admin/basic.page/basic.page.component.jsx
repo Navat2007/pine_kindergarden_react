2f865af2176338b368871e26d0fa4cc07c6fd80b
Navat2007 <navat2007@yandex.ru>
2023-09-25 18:39:25 +0300

    add

--- src/components/admin/basic.page/basic.page.component.jsx
+++ src/components/admin/basic.page/basic.page.component.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import TitleBlock from "../title.block/title.block.component";
 
-const BasicPage = ({children, id,  mainStore, loadings = [], back}) => {
+const BasicPage = ({children, id,  mainStore, loadings = [], back, loadingTimer = 500}) => {
+
+    const [basicLoading, setBasicLoading] = React.useState(false);
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setBasicLoading(true);
+        }, loadingTimer)
+    }, []);
 
     if(mainStore === undefined) {
         return <TitleBlock title={`Oшибка. Не удалось загрузить данные`}/>;
     }
 
-    const Loading = () => {
-        if (loadings.filter(item => item.loading).length > 0) {
-            return <TitleBlock title={`Загрузка...`}/>;
-        }
-    };
-
-    const NotFound = () => {
-        if (id && loadings.filter(item => item.loading).length === 0 && Object.keys(mainStore.item).length === 0) {
-            return <TitleBlock title={`Страница не найдена.`} onBack={back}/>;
-        }
-    };
-
-    return (
-        <>
-            <Loading />
-            <NotFound />
-            {children}
-        </>
-    );
+    if(basicLoading === false || loadings.filter(item => item.loading).length > 0) {
+        return <TitleBlock title={`Загрузка...`}/>;
+    }
+    else if(id && Object.keys(mainStore.item).length === 0)
+    {
+        return <TitleBlock title={`Страница не найдена.`} onBack={back}/>;
+    }
+    else {
+        return (
+            <>
+                {children}
+            </>
+        );
+    }
 };
 
 export default BasicPage;
