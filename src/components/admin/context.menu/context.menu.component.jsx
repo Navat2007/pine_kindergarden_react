85e0d4158b0e8907cd0c4d0d6ec19f8881c97815
Navat2007 <navat2007@yandex.ru>
2023-09-28 19:56:14 +0300

    add

--- src/components/admin/context.menu/context.menu.component.jsx
+++ src/components/admin/context.menu/context.menu.component.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import {AnimatePresence, motion} from "framer-motion";
+
 import Button from "../button/button.component";
 
 import {AdminIcons} from "../../svgs";
 
-{/* Здесь просится новый компонент - контекстное меню. Сейчас оно открыто и выпадает справа окна, но в будущем можно будет задавать позицию точнее.
-                    Появление можешь сделать через моушен. Класс admin-context-menu__wrapper при нажатии на кнопку будет открываться/скрываться.
-                    Также у класса admin-context-menu__item если он выбран появляется модификатор admin-context-menu__item_active */}
-
-{/* Конец контекстного меню. Когда вынесешь сообщи, я стили также перенесу. */}
-const ContextMenu = () => {
+const ContextMenu = ({items, onItemClick}) => {
     const [opened, setOpened] = React.useState(false);
+    const [activeItem, setActiveItem] = React.useState(0);
+
+    const variants = {
+        open: {
+            opacity: 1,
+            y: 0,
+        },
+        closed: {
+            opacity: 0,
+            y: -10,
+        },
+    };
 
     return (
         <nav className='admin-context-menu'>
@@ -18,30 +27,75 @@ const ContextMenu = () => {
                 isIconBtn={true}
                 extraClass={"admin-context-menu__menu-button"}
                 iconName={AdminIcons.view_list}
+                onClick={() => setOpened(!opened)}
             />
-            <div className='admin-context-menu__wrapper'>
-                <p className='admin-context-menu__title'>Вид</p>
-                <ul className='admin-context-menu__list'>
-                    <li className='admin-context-menu__item admin-context-menu__item_active'>
-                        <p className='admin-context-menu-item admin-context-menu-item_active'>
-                                        <span className='admin-context-menu-item__label'>
-                                            {AdminIcons.view_list}
-                                            Плитка
-                                        </span>
-                            <span className='admin-context-menu-item__icon'>{AdminIcons.check}</span>
-                        </p>
-                    </li>
-                    <li className='admin-context-menu__item admin-context-menu__item_active'>
-                        <p className='admin-context-menu-item'>
-                                        <span className='admin-context-menu-item__label'>
-                                            {AdminIcons.view_module}
-                                            Список
-                                        </span>
-                            <span className='admin-context-menu-item__icon'>{AdminIcons.check}</span>
-                        </p>
-                    </li>
-                </ul>
-            </div>
+            <motion.div animate={opened ? "open" : "closed"} variants={variants}>
+                <AnimatePresence>
+                    {opened && (
+                        <motion.div
+                            className={`search-filter__filed-columns`}
+                            initial={{
+                                height: 0,
+                                opacity: 0,
+                                y: -20,
+                            }}
+                            animate={{
+                                height: "auto",
+                                opacity: 1,
+                                y: 0,
+                                transition: {
+                                    height: {
+                                        duration: 0.25,
+                                    },
+                                    opacity: {
+                                        duration: 0.25,
+                                        delay: 0.15,
+                                    },
+                                },
+                            }}
+                            exit={{
+                                height: 0,
+                                opacity: 0,
+                                y: -20,
+                                transition: {
+                                    height: {
+                                        duration: 0.25,
+                                    },
+                                    opacity: {
+                                        duration: 0.15,
+                                    },
+                                },
+                            }}
+                        >
+                            <div className='admin-context-menu__wrapper'>
+                                <p className='admin-context-menu__title'>Вид</p>
+                                <ul className='admin-context-menu__list'>
+                                    {items.map((item, index) => (
+                                        <li
+                                            key={item.title}
+                                            className='admin-context-menu__item admin-context-menu__item_active'
+                                            onClick={() => {
+                                                setActiveItem(index);
+                                                onItemClick(index);
+                                                setOpened(!opened);
+                                            }}
+                                        >
+                                            <p className={`admin-context-menu-item${index === activeItem ? ` admin-context-menu-item_active` : ``}`}>
+                                            <span className='admin-context-menu-item__label'>
+                                                {item.icon}
+                                                {item.title}
+                                            </span>
+                                                <span className='admin-context-menu-item__icon'>{AdminIcons.check}</span>
+                                            </p>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </motion.div>
+
         </nav>
     );
 };
--- src/components/admin/file.selector.popup/file.selector.popup.jsx
+++ src/components/admin/file.selector.popup/file.selector.popup.jsx
@@ -5,7 +5,6 @@ import useMediaFilesStore from "../../../store/admin/mediaFilesStore";
 import SearchFilter from "../search.filter/search.filter.component";
 import ContextMenu from "../context.menu/context.menu.component";
 import Button from "../button/button.component";
-import Table from "../table/table.component";
 
 import "./file.selector.popup.scss";
 import { AdminIcons, FileIcons } from "../../svgs";
@@ -13,7 +12,7 @@ import { AdminIcons, FileIcons } from "../../svgs";
 const FileSelectorPopup = ({ onFileSelected }) => {
     const store = useMediaFilesStore();
 
-    const url = "admin/mediaFiles";
+    const [view, setView] = React.useState("list");
 
     const fetchData = async () => {
         await store.loadAll();
@@ -23,8 +22,14 @@ const FileSelectorPopup = ({ onFileSelected }) => {
         fetchData();
     }, []);
 
-    const onItemClick = (props) => {
-        console.log(props);
+    const onContextItemClick = (props) => {
+        if(props === 0){
+            setView("list");
+        }
+
+        if(props === 1){
+            setView("grid");
+        }
     };
 
     const itemConfig = [
@@ -57,78 +62,186 @@ const FileSelectorPopup = ({ onFileSelected }) => {
             sorting: true,
         },
     ];
+    const contextMenu = [
+        {
+            title: "Список",
+            icon: AdminIcons.view_list
+        },
+        {
+            title: "Плитка",
+            icon: AdminIcons.view_module
+        }
+    ]
 
     return (
         <div className='file-selector-popup'>
             <div className='file-selector-popup__search'>
                 <SearchFilter config={itemConfig}>
-                    <ContextMenu />
+                    <ContextMenu items={contextMenu} onItemClick={onContextItemClick}/>
                 </SearchFilter>
             </div>
-            <ol className='file-selector-popup__list'>
-                <li>
-                    <article class='file-selector-popup-card file-selector-popup-card_active'>
-                        {FileIcons.doc}
-                        <p class='file-selector-popup-card__title'>пояснения.docx</p>
-                    </article>
-                </li>
-                <li>
-                    <article class='file-selector-popup-card'>
-                        {FileIcons.pdf}
-                        <p class='file-selector-popup-card__title'>пояснения.docx</p>
-                    </article>
-                </li>
-                <li>
-                    <article class='file-selector-popup-card'>
-                        {FileIcons.xls}
-                        <p class='file-selector-popup-card__title'>
-                            Было бы странно обсуждать арии и музыку в разделе про доступность.xls
-                        </p>
-                    </article>
-                </li>
-                <li>
-                    <article class='file-selector-popup-card'>
-                        <img
-                            className='file-selector-popup-card__image'
-                            src='https://demotivation.ru/wp-content/uploads/2020/11/1553308805_fitnes_modeli_nike_ampquot_-_kartochka_polzovatelja_olgajarygina1992.jpg'
-                        />
-                        <p className='file-selector-popup-card__image-src'>
-                            https://demotivation.ru/wp-content/uploads/2020/11/1553308805_fitnes_modeli_nike_ampquot_-_kartochka_polzovatelja_olgajarygina1992.jpg
-                        </p>
-                    </article>
-                </li>
-            </ol>
-            {/* Если нужно плиткой, то следует добавить класс file-selector-popup__list_type_card-deck - на примере ниже. */}
-            <ol class='file-selector-popup__list file-selector-popup__list_type_card-deck'>
-                <li>
-                    <article class='file-selector-popup-card'>
-                        {FileIcons.doc}
-                        <p class='file-selector-popup-card__title'>пояснения.docx</p>
-                    </article>
-                </li>
-                <li>
-                    <article class='file-selector-popup-card'>
-                        {FileIcons.pdf}
-                        <p class='file-selector-popup-card__title'>пояснения.docx</p>
-                    </article>
-                </li>
-                <li>
-                    <article class='file-selector-popup-card'>
-                        {FileIcons.xls}
-                        <p class='file-selector-popup-card__title'>
-                            Было бы странно обсуждать арии и музыку в разделе про доступность.xls
-                        </p>
-                    </article>
-                </li>
-                <li>
-                    <article class='file-selector-popup-card'>
-                        <img
-                            className='file-selector-popup-card__image'
-                            src='https://demotivation.ru/wp-content/uploads/2020/11/1553308805_fitnes_modeli_nike_ampquot_-_kartochka_polzovatelja_olgajarygina1992.jpg'
-                        />
-                    </article>
-                </li>
-            </ol>
+            {
+                view === "list" &&
+                <ol className='file-selector-popup__list'>
+                    <li>
+                        <article className='file-selector-popup-card file-selector-popup-card_active'>
+                            {FileIcons.doc}
+                            <p className='file-selector-popup-card__title'>пояснения.docx</p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.pdf}
+                            <p className='file-selector-popup-card__title'>пояснения.docx</p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.xls}
+                            <p className='file-selector-popup-card__title'>
+                                Было бы странно обсуждать арии и музыку в разделе про доступность.xls
+                            </p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            <img
+                                className='file-selector-popup-card__image'
+                                src='https://demotivation.ru/wp-content/uploads/2020/11/1553308805_fitnes_modeli_nike_ampquot_-_kartochka_polzovatelja_olgajarygina1992.jpg'
+                            />
+                            <p className='file-selector-popup-card__image-src'>
+                                https://demotivation.ru/wp-content/uploads/2020/11/1553308805_fitnes_modeli_nike_ampquot_-_kartochka_polzovatelja_olgajarygina1992.jpg
+                            </p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.doc}
+                            <p className='file-selector-popup-card__title'>пояснения.docx</p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.pdf}
+                            <p className='file-selector-popup-card__title'>пояснения.docx</p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.xls}
+                            <p className='file-selector-popup-card__title'>
+                                Было бы странно обсуждать арии и музыку в разделе про доступность.xls
+                            </p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            <img
+                                className='file-selector-popup-card__image'
+                                src='https://demotivation.ru/wp-content/uploads/2020/11/1553308805_fitnes_modeli_nike_ampquot_-_kartochka_polzovatelja_olgajarygina1992.jpg'
+                            />
+                            <p className='file-selector-popup-card__image-src'>
+                                https://demotivation.ru/wp-content/uploads/2020/11/1553308805_fitnes_modeli_nike_ampquot_-_kartochka_polzovatelja_olgajarygina1992.jpg
+                            </p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.doc}
+                            <p className='file-selector-popup-card__title'>пояснения.docx</p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.pdf}
+                            <p className='file-selector-popup-card__title'>пояснения.docx</p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.xls}
+                            <p className='file-selector-popup-card__title'>
+                                Было бы странно обсуждать арии и музыку в разделе про доступность.xls
+                            </p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            <img
+                                className='file-selector-popup-card__image'
+                                src='https://demotivation.ru/wp-content/uploads/2020/11/1553308805_fitnes_modeli_nike_ampquot_-_kartochka_polzovatelja_olgajarygina1992.jpg'
+                            />
+                            <p className='file-selector-popup-card__image-src'>
+                                https://demotivation.ru/wp-content/uploads/2020/11/1553308805_fitnes_modeli_nike_ampquot_-_kartochka_polzovatelja_olgajarygina1992.jpg
+                            </p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.doc}
+                            <p className='file-selector-popup-card__title'>пояснения.docx</p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.pdf}
+                            <p className='file-selector-popup-card__title'>пояснения.docx</p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.xls}
+                            <p className='file-selector-popup-card__title'>
+                                Было бы странно обсуждать арии и музыку в разделе про доступность.xls
+                            </p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            <img
+                                className='file-selector-popup-card__image'
+                                src='https://demotivation.ru/wp-content/uploads/2020/11/1553308805_fitnes_modeli_nike_ampquot_-_kartochka_polzovatelja_olgajarygina1992.jpg'
+                            />
+                            <p className='file-selector-popup-card__image-src'>
+                                https://demotivation.ru/wp-content/uploads/2020/11/1553308805_fitnes_modeli_nike_ampquot_-_kartochka_polzovatelja_olgajarygina1992.jpg
+                            </p>
+                        </article>
+                    </li>
+                </ol>
+            }
+            {
+                view === "grid" &&
+                <ol className='file-selector-popup__list file-selector-popup__list_type_card-deck'>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.doc}
+                            <p className='file-selector-popup-card__title'>пояснения.docx</p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.pdf}
+                            <p className='file-selector-popup-card__title'>пояснения.docx</p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            {FileIcons.xls}
+                            <p className='file-selector-popup-card__title'>
+                                Было бы странно обсуждать арии и музыку в разделе про доступность.xls
+                            </p>
+                        </article>
+                    </li>
+                    <li>
+                        <article className='file-selector-popup-card'>
+                            <img
+                                className='file-selector-popup-card__image'
+                                src='https://demotivation.ru/wp-content/uploads/2020/11/1553308805_fitnes_modeli_nike_ampquot_-_kartochka_polzovatelja_olgajarygina1992.jpg'
+                            />
+                        </article>
+                    </li>
+                </ol>
+            }
             <div className='file-selector-popup__panel'>
                 <Button type='button' theme='text'>
                     Отмена
