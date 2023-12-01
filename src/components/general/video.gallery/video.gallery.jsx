31f96ee2a4aea1f6ac5567e0f46d5ec36ecd4519
Savallera <phenixvn1985@gmail.com>
2023-12-01 13:18:10 +0300

    стилизация блока Видео Галерея в кастомных страницах

--- src/components/general/video.gallery/video.gallery.jsx
+++ src/components/general/video.gallery/video.gallery.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 import VideoPlayer from "../../public/video.player/video.player.component";
-import {isArray} from "lodash";
-
-const VideoGallery = ({items}) => {
-    if(isArray(items) && items?.length > 0)
-    {
+import { isArray } from "lodash";
+import "./video.gallery.scss";
+const VideoGallery = ({ items }) => {
+    if (isArray(items) && items?.length > 0) {
         return (
-            <ul className='gallery-form'>
+            <ul className='video-gallery'>
                 {items.map((item) => (
-                    <li key={window.global.makeid(6)} className='gallery-form__item'>
+                    <li key={window.global.makeid(6)} className='video-gallery__item'>
                         <VideoPlayer source={item.url} />
                     </li>
                 ))}
--- src/pages/public/custom/custom.page.jsx
+++ src/pages/public/custom/custom.page.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import {Helmet} from "react-helmet";
-import {motion} from "framer-motion";
-import {useLocation} from "react-router-dom";
-import {isObject} from "lodash";
-import {computed} from "@preact/signals-react";
+import React from "react";
+import { Helmet } from "react-helmet";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
+import { isObject } from "lodash";
+import { computed } from "@preact/signals-react";
 import createDOMPurify from "dompurify";
 
 import useCustomPagesStore from "../../../store/public/customPagesStore";
@@ -13,26 +13,27 @@ import BasicPage from "../../../components/public/basic.page/basic.page.componen
 import ImageGallery from "../../../components/general/image.gallery/image.gallery.component";
 import VideoGallery from "../../../components/general/video.gallery/video.gallery";
 import Button from "../../../components/general/button/button.component";
+import Breadcrumbs from "../../../components/public/breadcrumbs/breadcrumbs";
 
-const CustomPage = ({id}) => {
+const CustomPage = ({ id }) => {
     const location = useLocation();
     const DOMPurify = createDOMPurify(window);
 
     const store = useCustomPagesStore();
 
     const photo = computed(() => {
-        return store.item?.page?.files?.filter(item => item.type === "photo") || [];
-    })
+        return store.item?.page?.files?.filter((item) => item.type === "photo") || [];
+    });
     const video = computed(() => {
-        return store.item?.page?.files?.filter(item => item.type === "video") || [];
-    })
+        return store.item?.page?.files?.filter((item) => item.type === "video") || [];
+    });
     const files = computed(() => {
-        return store.item?.page?.files?.filter(item => item.type === "file") || [];
-    })
+        return store.item?.page?.files?.filter((item) => item.type === "file") || [];
+    });
 
     React.useEffect(() => {
         if (id) {
-            store.loadByID({id});
+            store.loadByID({ id });
         }
     }, [location]);
 
@@ -41,65 +42,83 @@ const CustomPage = ({id}) => {
             <Helmet>
                 <title>{store.item?.menu?.title}</title>
             </Helmet>
-            {
-                (store.item?.page && isObject(store.item?.page) && (store.item?.page?.content || store.item?.page?.files?.length > 0))
-                    ?
-                    <motion.section
-                        className='article'
-                        initial={{opacity: 0}}
-                        animate={{opacity: 1}}
-                        exit={{opacity: 0}}
-                        transition={{delay: 0.2, duration: 1}}
-                    >
-                        <h1 className='article__title'>{store?.item?.page?.title}</h1>
-                        <div
-                            className='article__main-content'
-                            dangerouslySetInnerHTML={{
-                                __html: DOMPurify.sanitize(store?.item?.page?.content),
-                            }}
-                        />
-                        {
-                            photo.value.length > 0
-                            &&
-                            <ImageGallery extraClass={"article__gallery"} items={photo.value}/>
-                        }
-                        {
-                            video.value.length > 0
-                            &&
+            <Breadcrumbs
+                items={[
+                    {
+                        title: "Главная",
+                        url: "/",
+                    },
+                    {
+                        title: "Добавить навигацию",
+                        url: "/новости/",
+                    },
+                    {
+                        title: store.item.title,
+                        url: "",
+                    },
+                ]}
+            />
+            {store.item?.page &&
+            isObject(store.item?.page) &&
+            (store.item?.page?.content || store.item?.page?.files?.length > 0) ? (
+                <motion.section
+                    className='article'
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ delay: 0.2, duration: 1 }}
+                >
+                    <h1 className='article__title'>{store?.item?.page?.title}</h1>
+                    <div
+                        className='article__main-content'
+                        dangerouslySetInnerHTML={{
+                            __html: DOMPurify.sanitize(store?.item?.page?.content),
+                        }}
+                    />
+                    {photo.value.length > 0 && (
+                        <div className='article__gap'>
+                            <h2 className='article__subtitle'>Галерея</h2>
+                            <ImageGallery extraClass={"article__gallery"} items={photo.value} />
+                        </div>
+                    )}
+                    {video.value.length > 0 && (
+                        <div className='article__gap'>
+                            <h2 className='article__subtitle'>Видео</h2>
                             <VideoGallery items={video.value} />
-                        }
-                        {
-                            files.value.length > 0
-                            &&
-                            <>
-                                <ul>
-                                    {
-                                        files.value.map(file => (
-                                            <li key={window.global.makeid(6)}>
-                                                <a
-                                                   href={file.url.includes("http") ? file.url : process.env.REACT_APP_BASE_URL + file.url}
-                                                   target={"_blank"} rel='noreferrer noopener nofollow'>
-                                                    {file.title ? file.title : file.url}
-                                                </a>
-                                            </li>
-                                        ))
-                                    }
-                                </ul>
-                                <Button
-                                    type='button'
-                                    theme={"info_outline"}
-                                    style={{alignSelf: "flex-start", marginTop: "1em"}}
-                                    text={"Скачать все файлы"}
-                                    onClick={() => {
-
-                                    }}
-                                />
-                            </>
-                        }
-                    </motion.section>
-                    :
-                    <Construction/>
-            }
+                        </div>
+                    )}
+                    {files.value.length > 0 && (
+                        <>
+                            <ul>
+                                {files.value.map((file) => (
+                                    <li key={window.global.makeid(6)}>
+                                        <a
+                                            href={
+                                                file.url.includes("http")
+                                                    ? file.url
+                                                    : process.env.REACT_APP_BASE_URL + file.url
+                                            }
+                                            target={"_blank"}
+                                            rel='noreferrer noopener nofollow'
+                                        >
+                                            {file.title ? file.title : file.url}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                            <Button
+                                type='button'
+                                theme={"info_outline"}
+                                style={{ alignSelf: "flex-start", marginTop: "1em" }}
+                                text={"Скачать все файлы"}
+                                onClick={() => {}}
+                            />
+                        </>
+                    )}
+                </motion.section>
+            ) : (
+                <Construction />
+            )}
         </BasicPage>
     );
 };
